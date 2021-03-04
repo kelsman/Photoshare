143a6015ced41ceb8fabb43341a98171bc5fdb93
kelvin <osekelvin22@gmail.com>
2021-03-04 19:47:18 +0100

    added largescreen navbar

--- client/src/component/MenuButtons/index.js
+++ client/src/component/MenuButtons/index.js
@@ -3,11 +3,17 @@ import * as Icon from 'react-feather';
 import ProfileIcon from '../ProfileIcon';
 import './style.scss';
 
+import { useHistory } from 'react-router-dom';
+import * as Routes from '../routes'
+
+const history = useHistory()
+
 const Menu = () => {
+
     return (
         <div className="menu">
             <Icon.Home className="icon" />
-            <Icon.Compass className="icon" />
+            <Icon.Compass className="icon" onClick={() => { history.push(Routes.Explore) }} />
             <Icon.Heart className="icon" />
             <ProfileIcon iconSize="medium" />
             {/* profile icon */}
